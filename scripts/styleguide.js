0a206cbfbd9401dc66533e6734e0ccc6aca08bca
Alex <alex@sitepoint.com>
2019-05-23 15:03:49 +1000

    Styleguide additions

---
+++ scripts/styleguide.js
@@ -0,0 +1,61 @@
+/* ==== Typography label generator === */
+
+
+/*  Load an External CSS file via JS - so we only need to attach this one JS file */
+
+addCSS('https://cdn.staticaly.com/gist/alexmwalker/72cbe26c13f6531d2d2da2b2c15df22a/raw/adaaa5c224cf3a78a2f364707318493cd89e6b15/device-size.css');
+
+// Include CSS file
+function addCSS(filename){
+ var head = document.getElementsByTagName('head')[0];
+
+ var style = document.createElement('link');
+ style.href = filename;
+ style.type = 'text/css';
+ style.rel = 'stylesheet';
+ head.append(style);
+}
+
+// round number
+function precisionRound(number, precision) {
+  var factor = Math.pow(10, precision);
+  return Math.round(number * factor) / factor;
+}
+
+
+
+
+// BUILD the report UI units for each type sample with the class 'report'. Run once on load.
+function reportBuild() {
+
+  // Get a NodeList of all .report elements
+  const myReports = document.querySelectorAll(".report");
+  
+  myReports.forEach(function(myReport, index) {
+    myReport.innerHTML += '<span><input type="checkbox" checked /><ul><li class="type">Type:</li><li class="weight">Weight:</li><li class="size">Size:</li><li class="lineheight">Line-height:</li></span>';
+    console.log("Reporting: "+myReports[2]);
+  });
+
+
+
+// Change the text of multiple elements with a loop
+demoClasses.forEach(element => {
+  element.textContent = 'All demo classes updated.';
+});
+
+// Access the first element in the NodeList
+demoClasses[0];
+
+}
+
+
+
+
+reportBuild();
+//reportType();
+
+/*
+window.onresize = function() {
+  reportType();
+};
+*/
--- scripts/typography.js
+++ scripts/typography.js
@@ -5,7 +5,7 @@
 
 addCSS('https://cdn.staticaly.com/gist/alexmwalker/72cbe26c13f6531d2d2da2b2c15df22a/raw/adaaa5c224cf3a78a2f364707318493cd89e6b15/device-size.css');
 
-// Include CSS file
+// Include CSS file to support generated elements
 function addCSS(filename){
  var head = document.getElementsByTagName('head')[0];
 
@@ -21,17 +21,19 @@ function precisionRound(number, precision) {
   var factor = Math.pow(10, precision);
   return Math.round(number * factor) / factor;
 }
+  
 
 
 
 
-// BUILD the report UI units for each type sample with the class 'report'. Run once on load.
+// BUILD the report UI units for each type element tagged with the class 'report'. Run once on load.
 function reportBuild() {
-    const myReports = document.querySelectorAll(".report");
+  var myReports = document.querySelectorAll(".report");
 
   myReports.forEach(function(myReport, index) {
     myReport.innerHTML += '<span><input type="checkbox" checked /><ul><li class="type">Type:</li><li class="weight">Weight:</li><li class="size">Size:</li><li class="lineheight">Line-height:</li></span>';
-
+    //console.log(myElement.nodeName);  
+    console.log(myReports[1]);
   });
 
 }
@@ -39,20 +41,19 @@ function reportBuild() {
                    
 // UPDATE the text nodes on load and on each resize.                    
 function reportType() { 
-  const myElements = document.querySelectorAll(".report");
+  myElements = document.querySelectorAll(".report");
 
   myElements.forEach(function(myElement, index) {
  
     
-    style = window.getComputedStyle
-      ? getComputedStyle(myElement, null)
-      : myElement.currentStyle;
+    style = window.getComputedStyle ? getComputedStyle(myElement, null) : myElement.currentStyle;
     
-    console.log(myElement.nodeName);  
+    //console.log("myElement: "+myReports[2]);  
    
     
     var fn = myElement.nodeName;
-    var ff1 = style.fontFamily;     
+    var ff1 = style.fontFamily;  
+    console.log(ff1);    
     var fw1 = style.fontWeight; 
     var lh1 = style.lineHeight;
     
@@ -60,7 +61,8 @@ function reportType() {
     var fs = precisionRound(fs1);
     
     var targetEl =     myElement.querySelector(".type");
-    var targetSize =   myElement.querySelector(".size");
+    console.log("TargetEl: "+targetEl); 
+    var targetSize   =   myElement.querySelector(".size");
     var targetFamily = myElement.querySelector(".family");
     var targetWeight = myElement.querySelector(".weight");
     var targetlh = myElement.querySelector(".lineheight");
@@ -72,7 +74,7 @@ function reportType() {
     /*var newFamily = document.createElement('li');
     newFamily.className = "family";
     newFamily.innerHTML = "<b>" + ff1 + "</b>";*/
-    
+  
     var newLh = document.createElement('li');
     newLh.className = "lineheight";
     newLh.innerHTML = "Line-height: <b>" + lh1 + "</b>";
@@ -101,3 +103,6 @@ reportType();
 window.onresize = function() {
   reportType();
 };
+
+
+
