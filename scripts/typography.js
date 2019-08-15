d2894606901f77ed120f45195ec90df69dc5020b
Alex <alex@sitepoint.com>
2019-08-15 10:21:49 +1000

    Red bubble interaction updated.
    The Red bubble number on the library nav now 'pops' into position as soon as you add a new book to your Offline library. That read bubble will disappear and reset each time the user goes to their library.

--- scripts/typography.js
+++ scripts/typography.js
@@ -1,18 +1,17 @@
 /* ==== Typography label generator === */
 
 
-/*  Load an External CSS file via JS - so we only need to attach this one JS file */
+/*  Load an External CSS file via JS - only need this one file */
 
-addCSS('https://cdn.staticaly.com/gist/alexmwalker/72cbe26c13f6531d2d2da2b2c15df22a/raw/adaaa5c224cf3a78a2f364707318493cd89e6b15/device-size.css');
+addCSS("https://cdn.staticaly.com/gist/alexmwalker/72cbe26c13f6531d2d2da2b2c15df22a/raw/adaaa5c224cf3a78a2f364707318493cd89e6b15/device-size.css");
 
-// Include CSS file to support generated elements
+/* Include CSS file */
 function addCSS(filename){
- var head = document.getElementsByTagName('head')[0];
-
- var style = document.createElement('link');
+ var head = document.getElementsByTagName("head")[0];
+ var style = document.createElement("link");
  style.href = filename;
- style.type = 'text/css';
- style.rel = 'stylesheet';
+ style.type = "text/css";
+ style.rel = "stylesheet";
  head.append(style);
 }
 
@@ -21,88 +20,64 @@ function precisionRound(number, precision) {
   var factor = Math.pow(10, precision);
   return Math.round(number * factor) / factor;
 }
-  
 
-
-
-
-// BUILD the report UI units for each type element tagged with the class 'report'. Run once on load.
+// BUILD the report UI units for each type sample with the class 'report'. Run once on load.
 function reportBuild() {
   var myReports = document.querySelectorAll(".report");
-
+  var numberOfReports = myReports.length;
+  alert("Reports: " + numberOfReports);
   myReports.forEach(function(myReport, index) {
     myReport.innerHTML += '<span><input type="checkbox" checked /><ul><li class="type">Type:</li><li class="weight">Weight:</li><li class="size">Size:</li><li class="lineheight">Line-height:</li></span>';
-    //console.log(myElement.nodeName);  
-    console.log(myReports[1]);
   });
-
 }
-                
-                   
-// UPDATE the text nodes on load and on each resize.                    
-function reportType() { 
-  myElements = document.querySelectorAll(".report");
+/* UPDATE the text nodes on load and on each resize.*/
 
+function reportType(){
+  var myElements = document.querySelectorAll(".report");
   myElements.forEach(function(myElement, index) {
- 
-    
-    style = window.getComputedStyle ? getComputedStyle(myElement, null) : myElement.currentStyle;
-    
-    //console.log("myElement: "+myReports[2]);  
-   
-    
+    style = (window.getComputedStyle ? getComputedStyle(myElement, null) : myElement.currentStyle);
+    console.log(myElement.nodeName);
     var fn = myElement.nodeName;
-    var ff1 = style.fontFamily;  
-    console.log(ff1);    
-    var fw1 = style.fontWeight; 
+    var ff1 = style.fontFamily;
+    var fw1 = style.fontWeight;
     var lh1 = style.lineHeight;
-    
-    var fs1 = parseInt(style.fontSize); 
-    var fs = precisionRound(fs1);
-    
+    var fs1 = parseInt(style.fontSize);
+    var fs  = precisionRound(fs1);
     var targetEl =     myElement.querySelector(".type");
-    console.log("TargetEl: "+targetEl); 
-    var targetSize   =   myElement.querySelector(".size");
+    var targetSize =   myElement.querySelector(".size");
     var targetFamily = myElement.querySelector(".family");
     var targetWeight = myElement.querySelector(".weight");
-    var targetlh = myElement.querySelector(".lineheight");
-    
-    var newSize = document.createElement('li');
+    var targetlh =     myElement.querySelector(".lineheight");
+    var newSize = document.createElement("li");
     newSize.className = "size";
     newSize.innerHTML = "Size: <b>" + fs1 + "px</b>";
-    
-    /*var newFamily = document.createElement('li');
+
+    /*var newFamily = document.createElement("li");
     newFamily.className = "family";
     newFamily.innerHTML = "<b>" + ff1 + "</b>";*/
-  
-    var newLh = document.createElement('li');
+
+    var newLh = document.createElement("li");
     newLh.className = "lineheight";
     newLh.innerHTML = "Line-height: <b>" + lh1 + "</b>";
-    
-    var newWeight = document.createElement('li');
+
+    var newWeight = document.createElement("li");
     newWeight.className = "weight";
     newWeight.innerHTML = "Weight: <b>" + fw1 + "</b>";
 
-    var newType = document.createElement('li');
+    var newType = document.createElement("li");
     newType.className = "type";
-    newType.innerHTML = "<b>&nbsp;&lt;" + fn + "&gt;</b>";   
-    
-    targetEl.parentNode.replaceChild(newType, targetEl); 
-    targetSize.parentNode.replaceChild(newSize, targetSize);
-    //targetFamily.parentNode.replaceChild(newFamily, targetFamily);
-    targetlh.parentNode.replaceChild(newLh, targetlh);
-    targetWeight.parentNode.replaceChild(newWeight, targetWeight);    
-    // add the text node to the newly created div
+    newType.innerHTML = "<b>&nbsp;&lt;" + fn + "&gt;</b>";
 
+    targetEl.parentNode.replaceChild(newType, targetEl);
+    targetSize.parentNode.replaceChild(newSize, targetSize);
+    /*targetFamily.parentNode.replaceChild(newFamily, targetFamily);*/
+    targetlh.parentNode.replaceChild(newLh, targetlh);
+    targetWeight.parentNode.replaceChild(newWeight, targetWeight);
   });
 }
-
 reportBuild();
-reportType();
+//reportType();
 
 window.onresize = function() {
-  reportType();
+  //reportType();
 };
-
-
-
